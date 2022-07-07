95e701cba86ac3c9c2904611151e9bd61d20ff69
Allan Garcia <allan.garcia@gmail.com>
2022-07-07 01:50:02 +0000

    Solution to my first leet code

---
+++ 1-firstNonRepeatedNumberFunction.swift
@@ -0,0 +1,29 @@
+/*
+Problem: Write a function that give an array of numbers returns the first non-repeated number
+
+Ex. given the array [ 5, 1, 1, 5, 4, -1 ] -> the answer should be 4
+*/
+
+let arrayOfNumbers = [5, 1, 1, 5, 4, -1]
+
+func firstNonRepeatedNumber(of arrayOfNumbers: [Int]) -> Int? {
+
+  var seemBefore: [Int] = []
+
+  for i in arrayOfNumbers.indices {
+    let number = arrayOfNumbers[i]
+    if !seemBefore.contains(number) {
+      seemBefore.append(number)
+    } else {
+      seemBefore.removeAll(where: {$0 == number})
+    }
+  }
+
+  return(seemBefore.first)
+}
+
+if let number = firstNonRepeatedNumber(of: arrayOfNumbers) {
+  print("There is a response: \(number)")
+} else {
+  print("No non-repeating number.")
+}
