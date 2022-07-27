02b05d4a34e17ac56a2e20f97e520725d59d3c10
Allan Garcia <allan.garcia@gmail.com>
2022-07-27 20:42:45 -0300

    Create 8-firstNonRecurringChar.py

---
+++ 8-firstNonRecurringChar.py
@@ -0,0 +1,24 @@
+# Question variation: https://www.youtube.com/watch?v=GJdiM-muYqc
+#
+# Give the first NON repeating letter given a string of random letters
+#
+# Example: DBCABA -> B
+
+string = "BBAACCEEFOOPP"
+counter = {}
+
+def first_non_recurring_char_in(given_string):
+  for char in given_string:
+    if char in counter:
+      counter[char] += 1
+    else:
+      counter[char] = 1
+  
+  for char in given_string:
+    if counter[char] == 1:
+      return char
+  
+  return None
+
+result = first_non_recurring_char_in(string)
+print(result)
