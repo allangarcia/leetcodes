0e92965ea2a11794a3039d635a621f4bab01fc89
Allan Garcia <allan.garcia@gmail.com>
2022-08-06 22:26:48 +0000

    Note from crashes in codespaces

--- 5-waysToClimbAStair.swift
+++ 5-waysToClimbAStair.swift
@@ -11,6 +11,9 @@ For example, if N is 4, then there are 5 unique ways:
 2, 2
 What if, instead of being able to climb 1 or 2 steps at a time, you could climb any number from a set of positive 
 integers X? For example, if X = {1, 3, 5}, you could climb 1, 3, or 5 steps at a time. Generalize your function to take in X.
+
+NOTE: If too much output from the script codespaces crashes...
+
 */
 
 let n = 10
@@ -26,14 +29,14 @@ func waysToTraverseAStair(of size: Int, using steps: [Int]) -> [[Int]] {
         stack.append(item)
     }
 
-    print("Start stack: \(stack)")
+    //print("Start stack: \(stack)")
 
     while stack.count > 0 {
 
         let item = stack.popLast()
         if let possible = item {
             
-            print("Before for stack: \(stack)")
+            //print("Before for stack: \(stack)")
             for step in steps {
                 var list = possible
                 list.append(step)
@@ -41,9 +44,9 @@ func waysToTraverseAStair(of size: Int, using steps: [Int]) -> [[Int]] {
             }
             print("After for stack: \(stack)")
 
-            print("Before clean stack: \(stack)")
+            //print("Before clean stack: \(stack)")
             for item in stack {
-                print("Cleaning: \(item)")
+                //print("Cleaning: \(item)")
                 let sum = item.reduce(0, { $0 + $1 })
                 if sum == size {
                     results.append(item)
@@ -65,3 +68,4 @@ func waysToTraverseAStair(of size: Int, using steps: [Int]) -> [[Int]] {
 
 let results = waysToTraverseAStair(of: n, using: steps)
 print("Results: \(results)")
+print("Count: \(results.count)")
